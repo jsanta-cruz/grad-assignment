902efa804706ee4e14851c93251e54c8a5284729
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-27 15:33:12 -0400

    second commit

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-gatsby-js": () => import("./../../../src/pages/index-gatsby.js?export=default" /* webpackChunkName: "component---src-pages-index-gatsby-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+
+
+exports.head = {
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
+  "component---src-pages-index-gatsby-js": () => import("./../../../src/pages/index-gatsby.js?export=head" /* webpackChunkName: "component---src-pages-index-gatsby-jshead" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */)
+}
+
