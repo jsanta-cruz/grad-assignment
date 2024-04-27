db3648138b5466123049f68f3da5fe8036cc3c3d
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-27 16:00:02 -0400

    edited gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,25 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data/`,
+      },
     },
-    __key: "pages"
-  }]
+  //   "gatsby-plugin-mdx", {
+  //   resolve: 'gatsby-source-filesystem',
+  //   options: {
+  //     "name": "pages",
+  //     "path": "./src/pages/"
+  //   },
+  //   __key: "pages"
+  // }
+]
 };
---
+++ src/components/layout.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import { StaticImage } from 'gatsby-plugin-image';
+import { nav } from './layout.module.css';
+
+const Layout = ({ pageTitle, children }) => {
+
+        // Get the site title (data from gatsby-config.js)
+        const data = useStaticQuery(graphql`
+        query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        }
+    `)
+  return (
+    <div>
+      <header>
+        <div>
+        <Link to="/"><StaticImage src="../images/NFL_logo_PNG12.png" height={100} alt="NFL logo"></StaticImage></Link>
+        </div>
+        <h3>{data.site.siteMetadata.title}</h3>
+        <nav className={nav}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h2>{pageTitle}</h2>
+        {children}
+      </main>
+      <footer>
+                Example footer developed for CSCI E-114, Spring 2024.
+        </footer>
+    </div>
+  );
+};
+
+export default Layout;
