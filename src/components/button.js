b77cc0d5b452b6634260d4b24b9e0945265f730a
Joshua Santa-Cruz <Joshua.Santa-Cruz@acxiom.com>
2024-04-28 16:08:45 -0400

    added experiment to code but it's not working

--- src/components/button.js
+++ src/components/button.js
@@ -1,37 +1,73 @@
-import * as React from "react";
+// import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from 'react-dom/client';
 import { Link, graphql, useStaticQuery } from "gatsby";
 import "./button.module.css";
 
 // eslint-disable-next-line no-empty-pattern
-const Button = ({}) => {
-    const data = useStaticQuery(graphql`
-    query { pictures {
-        author
-        download_url
-        height
-        id
-        url
-        width
-      }
-    }
-    `);
-const imageEle = data.pictures.download_url
+const ImageButton = ( {pic} ) => {
+
   return (
-    <button
-      onClick={() => {
-    //     const num = Math.round(Math.random() * 32);
-    //    alert(data.pictures.download_url)
-    const root = createRoot(
-        document.getElementById('___gatsby')
-      );
-      const element = <img src={imageEle} width={600}></img>/*<h1>Hello, world</h1>;*/
-      root.render(element);
-      }}
-    >
-      Click me!
-    </button>
+    // <button
+    //   onClick={() => {
+    // const root = createRoot(
+    //     document.getElementById('___gatsby')
+    //   );
+    //   const element = <img src={imageEle} width={600}></img>
+    //   root.render(element);
+    //   }}
+    // >
+    //   Click me!
+    // </button>
+    <>
+    <h1>Hello World!</h1>
+    <Button />
+    </>
   );
 };
 
-export default Button;
+function Button( { pic } ) {
+    // const data = useStaticQuery(graphql`
+    // query { pictures {
+    //     author
+    //     download_url
+    //     height
+    //     id
+    //     url
+    //     width
+    //   }
+    // }
+    // `);
+//  const imageEle = data.pictures.download_url
+const [count, setCount] = useState(0);
+
+    return (
+    <button
+      onClick={() => { setCount(count + 1)
+      const root = createRoot(document.getElementById('___gatsby')
+          );
+      const element =  <ul>
+       {pic?.map((p, i) => (
+            <li key={i}>
+                <div>
+                <div>{p?.author}</div>
+                <img loading="lazy" src={p?.download_url} width={100} alt={p?.author} />
+                </div>
+            </li>
+    ))}
+      </ul>
+      root.render(element);
+    // const root = createRoot(
+    //     document.getElementById('___gatsby')
+    //   );
+    //   const element = <img src={imageEle} width={600}></img>
+    //   root.render(<ImageButton />);
+    
+      }}
+    >
+      You clicked me {count} times
+    </button>
+    )
+}
+
+export default ImageButton;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
 import Layout from "../components/layout";
-import Button from "../components/button";
+import ImageButton from "../components/button";
 import { Link, graphql, useStaticQuery } from "gatsby";
 
-const IndexPage = ({ data }) => {
-
-
+const IndexPage = (props) => {
+    const pictures = props.data.allPictures.nodes;
+    // const root = createRoot(document.getElementById('___gatsby'));
+    // root.render(<ImageButton />);
+//     const data = useStaticQuery(graphql`
+//     query { pictures {
+//         author
+//         download_url
+//         height
+//         id
+//         url
+//         width
+//       }
+//     }
+//     `);
+//  const imageEle = data.pictures.download_url
   return (
     <Layout>
       <main>
@@ -14,7 +27,22 @@ const IndexPage = ({ data }) => {
           alt="Gatsby G Logo"
           src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
         />
-        <Button />
+        {/* <Button /> */}
+        {/* const root = createRoot(document.getElementById('___gatsby'));
+      root.render(<ImageButton />); */}
+      <ul >
+        {pictures.map((pic) => {
+          return (
+            <li key={pic.id}>
+                <div>
+                {/* <div>{pic.author}</div>
+                <img loading="lazy" src={pic?.download_url} width={100} alt={pic.author} /> */}
+                </div>
+            </li>
+          );
+        })}
+      </ul>
+      <ImageButton />
       </main>
     </Layout>
   );
